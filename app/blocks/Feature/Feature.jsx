a409bff9dfad69331137836f41d06f8b9d5d753b
Tomek Rozalski <github@tomaszrozalski.pl>
2022-04-26 15:23:26 +0200

    Add responsive image support

--- app/blocks/Feature/Feature.jsx
+++ app/blocks/Feature/Feature.jsx
@@ -1,4 +1,5 @@
 import { storyblokEditable } from "@storyblok/react";
+import Image from "remix-image";
 
 import styles from "./feature.css";
 
@@ -7,7 +8,19 @@ export const links = () => [{ href: styles, rel: "stylesheet" }];
 const Feature = ({ blok }) => {
   return (
     <div className="feature" {...storyblokEditable(blok)} key={blok._uid}>
-      {blok.name}
+      <div>{blok.name}</div>
+      <Image
+        alt={blok.image.alt ?? ""}
+        className="feature__image"
+        src={blok.image.filename}
+        responsive={[
+          { size: { width: 400 } },
+          { size: { width: 600 } },
+          { size: { width: 800 } },
+          { size: { width: 1000 } },
+          { size: { width: 1200 } },
+        ]}
+      />
     </div>
   );
 };
